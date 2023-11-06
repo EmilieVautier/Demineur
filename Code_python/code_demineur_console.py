14d7861edc1fc74e0dfac5cd4902e70c2fc4c010
frederic vautier <emilie.vautier1@gmail.com>
2023-11-06 14:28:36 +0100

    Commit class diagram v2

--- Code_python/code_demineur_console.py
+++ Code_python/code_demineur_console.py
@@ -1,9 +1,11 @@
 import random
+import time
 
 class Joueur:
     def __init__(self, nom):
         self.nom = nom
         self.score = 0
+        
 
 class Case:
     def __init__(self, x, y):
@@ -13,7 +15,6 @@ class Case:
         self.est_minee = False
         self.est_marquee = False
         self.nb_mines_voisines = 0
-        self.nb_ouverts_voisins = 0
 
     def ouvrir_case(self):
         if not self.est_ouverte:
@@ -75,7 +76,6 @@ class Grille:
             
     def ouvrir_zone(self, x, y):
         directions = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1]]
-        #random.shuffle(directions)
         for dx, dy in directions:
             
             alea = random.randint(0,self.dimensions[0]//2)
@@ -100,102 +100,181 @@ class Partie:
         self.grille = Grille(difficulte)
         self.en_cours = True
         self.nb_tentatives = 0
-
-    def lancer_partie(self):
-        premier_tour = True
-        while self.en_cours:
-            # Afficher la grille.
-            def afficher_grille():
-                for ligne in self.grille.cases:
-                    for case in ligne:
-                        if case.est_ouverte:
-                            print(case.nb_mines_voisines, end=' ')
-                        if case.est_marquee and case.est_ouverte==False:
-                            print('X', end=' ')
-                        if case.est_ouverte==False and case.est_marquee==False:
-                            print('+', end=' ')
-                    print()
-            afficher_grille()    
-                
-            # Demander si le joueur veux poser un drapeau
-            while self.nb_tentatives > 0:
-                choix = input("Voulez-vous poser un drapeau ? (O/N) ")
-                
-                if choix == 'O':
-                    x, y = map(int, input("Entrez les coordonnées de la case à marquer (x y): ").split())
-                    case_a_marquer = self.grille.cases[x][y]
-                    case_a_marquer.poser_drapeau()
-                    afficher_grille()
-                    if case_a_marquer.est_ouverte:
-                        print ("Cette case est déjà ouverte, inutile de la marquer!")
-                    # else:
-                    #     print("On continue!")
-                    # break
-                elif choix == 'N':
-                    print("On continue!")
-                    break
-                else:
-                    print("Répondez avec 'O' pour Oui ou 'N' pour Non.")
-                
-                    
-            # Demander au joueur d'entrer les coordonnées de la case à ouvrir.
-            x, y = map(int, input("Entrez les coordonnées de la case à ouvrir (x y): ").split())
-            case_a_ouvrir = self.grille.cases[x][y]
+        self.selected_x = None
+        self.selected_y = None
+    
+    
+    
+    
+    def score(self, debut):
+        fin = time.time()
+        self.joueur.score = round((10000**self.grille.difficulte/round(fin - debut)))
+    
+    def afficher_grille(self):
+        for ligne in self.grille.cases:
+            for case in ligne:
+                if case.est_ouverte:
+                    print(case.nb_mines_voisines, end=' ')
+                if case.est_marquee and case.est_ouverte==False:
+                    print('X', end=' ')
+                if case.est_ouverte==False and case.est_marquee==False:
+                    print('?', end=' ')
+            print()
             
-            # Si c'est le premier tour et que la case est minée, échanger la mine avec une autre case.
-            if premier_tour and case_a_ouvrir.est_minee:
-                while True:
-                    x2, y2 = random.randint(0, self.grille.dimensions[0] - 1), random.randint(0, self.grille.dimensions[1] - 1)
-                    if not self.grille.cases[x2][y2].est_minee:
-                        case_a_ouvrir.est_minee = False
-                        self.grille.cases[x2][y2].est_minee = True
-                        break
-                    
-            # Compter le nombre de mines voisines et ouvrir la case.
-            if not case_a_ouvrir.est_ouverte:  
-                case_a_ouvrir.compter_mines_voisines(self.grille)
-                case_a_ouvrir.ouvrir_case()
-
-            # Si c'est le premier tour, ouvrir une zone non minée autour de la case.
-            if premier_tour:
-                zone_size = random.randint(1, self.grille.dimensions[0]*self.grille.dimensions[1]//2)
-                for _ in range(zone_size):
-                    self.grille.ouvrir_zone(x, y)
-                    
-                self.grille.placer_mines(self.grille.nb_mines)
-                for x in range(self.grille.dimensions[0]):
-                    for y in range(self.grille.dimensions[1]):
-                        case = self.grille.cases[x][y]
-                        if case.est_ouverte:
-                            case.compter_mines_voisines(self.grille)
-                premier_tour = False
+    def choisir_drapeau(self):
+        # Demander si le joueur veux poser un drapeau
+        nom = self.joueur.nom
+        while self.nb_tentatives > 0:
+            choix = input("Voulez-vous poser un drapeau ? (O/N) ")
             
-            
-            
-            # Compter le nombre de mines voisines.
-            if not case_a_ouvrir.est_ouverte:  
-                case_a_ouvrir.compter_mines_voisines(self.grille)
-            if case_a_ouvrir.est_ouverte and self.nb_tentatives>0: 
-                print("Cette case est deja ouverte!")
-            
-            
-            # Ouvrir case
-            case_a_ouvrir.ouvrir_case()
-            
-            
-            # Vérifier si le joueur a gagné ou perdu.
-            if case_a_ouvrir.est_minee:
-                self.grille.ouvrir_toutes_les_cases()
-                print("BOOOOM!\nVous avez perdu!")
+            if choix == 'O':
+                x, y = map(int, input("Entrez les coordonnées de la case à marquer (x y): ").split())
+                case_a_marquer = self.grille.cases[x][y]
+                case_a_marquer.poser_drapeau()
+                self.afficher_grille()
+                if case_a_marquer.est_ouverte:
+                    print ("Cette case est déjà ouverte, inutile de la marquer!")
+                # else:
+                #     print("On continue!")
+                # break
+            elif choix == 'N':
+                print(f"On continue {nom}!")
                 break
             else:
-                nb_cases_non_minees_ouvertes = sum(case.est_ouverte and not case.est_minee for ligne in self.grille.cases for case in ligne)
-                if nb_cases_non_minees_ouvertes == len(self.grille.cases) * len(self.grille.cases[0]) - self.grille.nb_mines:
-                    print("Vous avez gagné!")
+                print("Répondez avec 'O' pour Oui ou 'N' pour Non.")
+    
+    def gagner_perdre(self,case_a_ouvrir,debut):
+        nom = self.joueur.nom
+        # Vérifier si le joueur a gagné ou perdu.
+        if case_a_ouvrir.est_minee:
+            self.grille.ouvrir_toutes_les_cases()
+            print(f"BOOOOM!!!!!!!\nVous avez perdu {nom}! :(")
+            return True
+        else:
+            nb_cases_non_minees_ouvertes = sum(case.est_ouverte and not case.est_minee for ligne in self.grille.cases for case in ligne)
+            if nb_cases_non_minees_ouvertes == len(self.grille.cases) * len(self.grille.cases[0]) - self.grille.nb_mines:
+                # end = time.time()
+                # self.joueur.score = round((10000**self.grille.difficulte/round(end - start)))
+                self.score(debut)
+                
+                print(f"Vous avez gagné {nom}!\nVotre score est de {self.joueur.score} :)")
+                return True
+    def choisir_case(self, premier_tour):
+        # Demander au joueur d'entrer les coordonnées de la case à ouvrir.
+        x, y = map(int, input("Entrez les coordonnées de la case à ouvrir (x y): ").split())
+        self.selected_x = x
+        self.selected_y = y
+        case_a_ouvrir = self.grille.cases[x][y]
+
+        # Si c'est le premier tour et que la case est minée, échanger la mine avec une autre case.
+        if premier_tour and case_a_ouvrir.est_minee:
+            while True:
+                x2, y2 = random.randint(0, self.grille.dimensions[0] - 1), random.randint(0, self.grille.dimensions[1] - 1)
+                if not self.grille.cases[x2][y2].est_minee:
+                    case_a_ouvrir.est_minee = False
+                    self.grille.cases[x2][y2].est_minee = True
                     break
+
+        # Compter le nombre de mines voisines et ouvrir la case.
+        if not case_a_ouvrir.est_ouverte:
+            case_a_ouvrir.compter_mines_voisines(self.grille)
+            case_a_ouvrir.ouvrir_case()
+        else:
+            print("Cette case est déjà ouverte!")
+
+        # Si c'est le premier tour, ouvrir une zone non minée autour of the case.
+        if premier_tour:
+            zone_size = random.randint(1, self.grille.dimensions[0] * self.grille.dimensions[1] // 2)
+            for _ in range(zone_size):
+                self.grille.ouvrir_zone(x, y)
+
+            self.grille.placer_mines(self.grille.nb_mines)
+            for x in range(self.grille.dimensions[0]):
+                for y in range(self.grille.dimensions[1]):
+                    case = self.grille.cases[x][y]
+                    if case.est_ouverte:
+                        case.compter_mines_voisines(self.grille)
+            premier_tour = False
+
+        # Ouvrir case
+        case_a_ouvrir.ouvrir_case()
+
+        return premier_tour
+    
+    def jouer_partie(self):
+        premier_tour = True
+        nom = self.joueur.nom
+
+        while self.en_cours:
+            debut = time.time()
+
+            self.afficher_grille()
+            self.choisir_drapeau()
+
+            # Call the choisir_case method to handle case selection and actions.
+            premier_tour = self.choisir_case(premier_tour)
+
+            finir = self.gagner_perdre(self.grille.cases[self.selected_x][self.selected_y], debut)
+            if finir == True:
+                break
+
             self.nb_tentatives += 1
+        # premier_tour = True
+        # nom = self.joueur.nom
+        
+        # while self.en_cours:
+        #     debut = time.time()
+            
+            
+        #     self.afficher_grille()    
+        #     self.choisir_drapeau()   
+                    
+        #     # Demander au joueur d'entrer les coordonnées de la case à ouvrir.
+        #     x, y = map(int, input("Entrez les coordonnées de la case à ouvrir (x y): ").split())
+        #     case_a_ouvrir = self.grille.cases[x][y]
+            
+        #     # Si c'est le premier tour et que la case est minée, échanger la mine avec une autre case.
+        #     if premier_tour and case_a_ouvrir.est_minee:
+        #         while True:
+        #             x2, y2 = random.randint(0, self.grille.dimensions[0] - 1), random.randint(0, self.grille.dimensions[1] - 1)
+        #             if not self.grille.cases[x2][y2].est_minee:
+        #                 case_a_ouvrir.est_minee = False
+        #                 self.grille.cases[x2][y2].est_minee = True
+        #                 break
+                    
+        #     # Compter le nombre de mines voisines et ouvrir la case.
+        #     if not case_a_ouvrir.est_ouverte:  
+        #         case_a_ouvrir.compter_mines_voisines(self.grille)
+        #         case_a_ouvrir.ouvrir_case()
+        #     else :
+        #         print("Cette case est deja ouverte!")
 
+        #     # Si c'est le premier tour, ouvrir une zone non minée autour de la case.
+        #     if premier_tour:
+        #         zone_size = random.randint(1, self.grille.dimensions[0]*self.grille.dimensions[1]//2)
+        #         for _ in range(zone_size):
+        #             self.grille.ouvrir_zone(x, y)
+                    
+        #         self.grille.placer_mines(self.grille.nb_mines)
+        #         for x in range(self.grille.dimensions[0]):
+        #             for y in range(self.grille.dimensions[1]):
+        #                 case = self.grille.cases[x][y]
+        #                 if case.est_ouverte:
+        #                     case.compter_mines_voisines(self.grille)
+        #         premier_tour = False
+            
+            
+        #     # Ouvrir case
+        #     case_a_ouvrir.ouvrir_case()
+            
+        #     finir = self.gagner_perdre(case_a_ouvrir,debut)
+        #     if finir==True:
+        #         break
+                
+          
+        #     self.nb_tentatives += 1
 
+        
 
 class Controle:
     def __init__(self):
@@ -204,12 +283,13 @@ class Controle:
     def lancer_partie(self):
         nom_joueur = input("Entrez votre nom: ")
         joueur = Joueur(nom_joueur)
-        print(f"Bienvenue, {nom_joueur}!\nVeuillez choisir une difficulté :\n1 - Facile\n2 - Moyen\n3 - Difficile")
+        print(f"Bienvenue {nom_joueur}!\nVeuillez choisir une difficulté :\n1 - Facile\n2 - Moyen\n3 - Difficile")
         difficulte = int(input("Choisissez la difficulté (1-3): "))
         
         partie = Partie(joueur, difficulte)
         
-        partie.lancer_partie()
+        partie.jouer_partie()
+
 
 if __name__ == "__main__":
     controle = Controle()
